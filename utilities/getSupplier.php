5a9e1b75eb4ff4621fa4b1dc8e400b70797163a8
KarusalaDeepak <77612743+KarusalaDeepak@users.noreply.github.com>
2022-01-30 22:00:21 +0530

    Update getSupplier.php

--- utilities/getSupplier.php
+++ utilities/getSupplier.php
@@ -41,6 +41,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+     <iframe name="DummyFrame" style="display:none;"></iframe>
 </body>
-    <iframe name="DummyFrame" style="display:none;"></iframe>
 </html>
